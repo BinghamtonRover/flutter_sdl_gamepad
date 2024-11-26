6c65bec4deef53753dc0d4602d52839201403a3e
Tymur Bondar <tymurbondar@outlook.com>
2024-11-25 19:46:56 -0500

    add macos support

--- lib/flutter_sdl_gamepad.dart
+++ lib/flutter_sdl_gamepad.dart
@@ -1,5 +1,14 @@
+import "dart:io";
+import "package:sdl3/sdl3.dart" as sdl;
 export "package:sdl_gamepad/sdl_gamepad.dart";
 
 class FlutterSdlGamepad {
-  static void registerWith() { }
+  static void registerWith() {
+    if (Platform.isMacOS){
+      // look at flutter_sdl_gamepad
+    final path = Platform.environment["SDLGAMEPAD_PATH"];
+    sdl.openLib(path!);
+    }
+  }
 }
+
---
+++ macos/Classes/flutter_sdl_gamepad.c
@@ -0,0 +1,5 @@
+// // Relative import to be able to reuse the C sources.
+// // See the comment in ../flutter_sdl_gamepad.podspec for more information.
+// #include "../../src/flutter_sdl_gamepad.c"
+// #include "../../src/flutter_sdl_gamepad.h"
+// #include <SDL3/SDL.h>
