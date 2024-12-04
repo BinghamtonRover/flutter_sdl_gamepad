0801116631636d04294e56f8ecf41b03525ee4ff
Levi Lesches <levilesches@gmail.com>
2024-12-04 08:05:22 -0500

    Fix Pub score issues (#2)
    * dart format
    
    * Added example
    
    * Updated description
    
    * Hide undocumented sdl members
    
    * Bumped version
    
    * Ran dart format

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_sdl_gamepad
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSdlGamepadPlugin.register(with: registry.registrar(forPlugin: "FlutterSdlGamepadPlugin"))
+}
--- lib/flutter_sdl_gamepad.dart
+++ lib/flutter_sdl_gamepad.dart
@@ -2,15 +2,17 @@ import "dart:io";
 import "dart:ffi";
 
 import "package:sdl3/sdl3.dart" as sdl;
-import "package:sdl_gamepad/sdl_gamepad.dart";
 
-export "package:sdl_gamepad/sdl_gamepad.dart";
+// For some reason, Pub thinks SdlGuid not being documented is our fault
+import "package:sdl_gamepad/sdl_gamepad.dart" hide SdlGuid;
+
+export "package:sdl_gamepad/sdl_gamepad.dart" hide SdlGuid;
 
 /// The wrapper plugin. Use [SdlGamepad] directly.
 class FlutterSdlGamepad {
   /// Flutter will call this method on launch. On Mac, loads the dylib needed by SDL3.
   static void registerWith() {
-    if (Platform.isMacOS){
+    if (Platform.isMacOS) {
       final path = Platform.environment["SDLGAMEPAD_PATH"];
       final lib = DynamicLibrary.open(path!);
       sdl.SdlDynamicLibraryService().add("SDL3", lib);
